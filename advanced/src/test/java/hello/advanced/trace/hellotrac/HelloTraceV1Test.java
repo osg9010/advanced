9db0333a911e367fbd528776ff39ed483755d364
osg9010 <osg3419010@naver.com>
2024-06-23 11:39:31 +0900

    스프링 핵심 원리
    로그 추적기 V1 - 프로토타입 개발, 테스트 진행

---
+++ advanced/src/test/java/hello/advanced/trace/hellotrac/HelloTraceV1Test.java
@@ -0,0 +1,25 @@
+package hello.advanced.trace.hellotrac;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end(){
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus hello = trace.begin("hello");
+        trace.end(hello);
+    }
+
+    @Test
+    void begin_exception(){
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status,new IllegalArgumentException());
+    }
+
+
+}
