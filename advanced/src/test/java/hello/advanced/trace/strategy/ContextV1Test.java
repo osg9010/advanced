dd038b8071de9652f76bb99feaba926097416578
osg9010 <osg3419010@naver.com>
2024-07-06 10:03:34 +0900

    스프링 핵심 원리 (전략패턴)
    예제1
    - 전략패턴 적용하여 테스트
    - Strategy라는 인터페이스를 만들고 해당 인터페이스를 구현한 클래스들로 안정적인 상태를 만듦
    - ContextV1이라는 클래스를 만들어 Strategy 구현체를 주입받아 해당 인터페이스의 비즈니스 로직을 실행하는 전략패턴을 구현

--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.ContextV1;
+import hello.advanced.trace.strategy.code.StrategyLogic1;
+import hello.advanced.trace.strategy.code.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +28,16 @@ public class ContextV1Test {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("졸료시간 = {}",resultTime);
+    }
+
+    @Test
+    void strategyV1(){
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
     }
 }
