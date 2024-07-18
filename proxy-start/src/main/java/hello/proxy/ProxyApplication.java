12084a5420cc52cd1b22d373879a139775236e6c
osg9010 <osg3419010@naver.com>
2024-07-18 11:22:36 +0900

    예제 만들기 v3
    - 인터페이스없이 구체클래스를 만들어 자동으로 빈등록 후 확인

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-//@Import(AppV1Config.class)
-@Import(AppV2Config.class)
+@Import({AppV2Config.class,AppV1Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
