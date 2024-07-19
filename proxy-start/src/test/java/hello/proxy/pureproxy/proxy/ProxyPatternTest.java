6d7abfe948c8abeb620e390f357de884d2715012
osg9010 <osg3419010@naver.com>
2024-07-19 10:42:34 +0900

    프록시패턴 (접근제어, 캐시)
    - 프록시패턴 테스트를 위한 클래스 생성

---
+++ proxy-start/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,20 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest(){
+        Subject realSubject = new RealSubject();
+
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+
+    }
+}
