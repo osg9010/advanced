bc9db10c8b804a746dd516b2e02e4abfc5bf8403
osg9010 <osg3419010@naver.com>
2024-07-20 09:59:33 +0900

    데코레이터 패턴(예제3) - 데코레이터 패턴(MessageDecorator,TimeDecorator) 적용
    - 데코레이터 패턴 : 클라이언트 -> 컴포넌트 <- 리얼컴포넌트
    - 예제3 : 클라이언트 -> TimeDecorator -> MessageDecorator ->RealComponent

--- proxy-start/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy-start/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -3,6 +3,7 @@ package hello.proxy.pureproxy.decorator;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import hello.proxy.pureproxy.proxy.code.MessageDecorator;
+import hello.proxy.pureproxy.proxy.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,15 @@ public class DecoratorPatternTest {
         RealComponent component = new RealComponent();
         MessageDecorator messageDecorator = new MessageDecorator(component);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2(){
+        RealComponent component = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(component);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 }
