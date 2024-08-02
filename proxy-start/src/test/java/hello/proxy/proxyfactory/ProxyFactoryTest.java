4074a8c8d98f0915be6eff59f8a48671379c13f6
osg9010 <osg3419010@naver.com>
2024-08-02 14:53:56 +0900

    스프링이 지원하는 프록시 - 예제 2
    - 스프링이 지원하는 동적 프록시 MethodInterceptor (org.aopalliance.intercept.MethodInterceptor) 는 인터페이스가 있을경우 jdkDynamicProxy로 구체 클래스만 있을경우에는 cglibProxy로 프록시를 생성해준다.
    
    - setProxyTargetClass(true);를 주면 인터페이스 기반 클래스여도   cglib 사용
    - 스프링 부트는 AOP를 적용할 때 (2.0부터) 기본적으로 proxyTargetClass=true 로 설정해서 사용한다

--- proxy-start/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ proxy-start/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package hello.proxy.proxyfactory;
 
 import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import jdk.jfr.Name;
@@ -32,6 +33,43 @@ public class ProxyFactoryTest {
         Assertions.assertThat(aopProxy).isTrue();
         Assertions.assertThat(aopProxy2).isTrue();
         Assertions.assertThat(aopProxy3).isFalse();
+    }
+    @Test
+    @DisplayName("구체 클래스만 있으면 cglib 프록시 사용")
+    void concreteProxy(){
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass = {}",target.getClass());
+        log.info("proxyClass = {}",proxy.getClass());
+        proxy.call();
+
+        boolean aopProxy = AopUtils.isAopProxy(proxy);
+        boolean aopProxy2 = AopUtils.isCglibProxy(proxy);
+        boolean aopProxy3 = AopUtils.isJdkDynamicProxy(proxy);
+
+        Assertions.assertThat(aopProxy).isTrue();
+        Assertions.assertThat(aopProxy2).isTrue();
+        Assertions.assertThat(aopProxy3).isFalse();
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 있으면 인터페이스 기반 클래스여도   cglib 사용")
+    void proxyTargetClass(){
+        ServiceInterface service = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(service);
+        proxyFactory.addAdvice(new TimeAdvice());
+        proxyFactory.setProxyTargetClass(true);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass = {}",service.getClass());
+        log.info("proxyClass = {}",proxy.getClass());
+        proxy.find();
+
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
 
 }
